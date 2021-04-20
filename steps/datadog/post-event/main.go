7e925159f23e2f76a6f61d992eaf771b6fc23408
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-20 17:09:50 +0200

    review changes

--- steps/datadog/post-event/main.go
+++ steps/datadog/post-event/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/stackpulse/steps-sdk-go/env"
@@ -15,26 +16,17 @@ import (
 const stackpulseSource = "STACKPULSE"
 
 type EventPost struct {
-	ApiKey         string   `env:"API_KEY,required"`
-	UseEu          bool     `env:"USE_EU"`
+	ApiKey         string   `env:"DD_API_KEY,required"`
+	Site           bool     `env:"DD_SITE"`
 	Title          string   `env:"TITLE,required"`
 	Text           string   `env:"TEXT,required"`
 	Tags           []string `env:"TAGS"`
 	AggregationKey string   `env:"AGGREGATION_KEY"`
-	RelatedEventId int      `env:"RELATED_EVENT_ID"`
-}
-
-type createRequest struct {
-	Title          string   `json:"title"`
-	Text           string   `json:"text"`
-	Tags           []string `json:"tags,omitempty"`
-	AggregationKey string   `json:"aggregation_key,omitempty"`
-	RelatedEventId int      `json:"related_event_id,omitempty"`
-	SourceTypeName string   `json:"source_type_name,omitempty"`
+	RelatedEventId int64    `env:"RELATED_EVENT_ID"`
 }
 
 type output struct {
-	ID int `json:"id"`
+	ID int64 `json:"id"`
 	step.Outputs
 }
 
@@ -43,34 +35,18 @@ func (s *EventPost) Init() error {
 	if err != nil {
 		return err
 	}
-
 	return nil
-}
-
-func (s *EventPost) getURL(useEU bool) string {
-	hostExt := "com"
-	if useEU {
-		hostExt = "eu"
-	}
-	return fmt.Sprintf("https://api.datadoghq.%s/api/v1/events", hostExt)
 }
 
 func (s *EventPost) Run() (int, []byte, error) {
 	//prepare request body
-	Title := s.Title
-	Text := s.Text
-	Tags := s.Tags
-	AggregationKey := s.AggregationKey
-	RelatedEventId := int64(s.RelatedEventId)
-	stackpulseSource := string(stackpulseSource)
-
 	body := datadog.EventCreateRequest{
-		Title:          Title,
-		Text:           Text,
-		Tags:           &Tags,
-		AggregationKey: &AggregationKey,
-		RelatedEventId: &RelatedEventId,
-		SourceTypeName: &stackpulseSource,
+		Title:          s.Title,
+		Text:           s.Text,
+		Tags:           &s.Tags,
+		AggregationKey: &s.AggregationKey,
+		RelatedEventId: &s.RelatedEventId,
+		SourceTypeName: datadog.PtrString(stackpulseSource),
 	}
 
 	ctx := context.WithValue(
@@ -87,33 +63,33 @@ func (s *EventPost) Run() (int, []byte, error) {
 
 	// send request
 	apiClient := datadog.NewAPIClient(configuration)
-	responseBody, r, err := apiClient.EventsApi.CreateEvent(ctx).Body(body).Execute()
+	resp, r, err := apiClient.EventsApi.CreateEvent(ctx).Body(body).Execute()
+	if err != nil {
+		if r.StatusCode != http.StatusAccepted {
+			return step.ExitCodeFailure, nil, fmt.Errorf("failed to create event. got response code: %d",
+				r.Status)
+		} else {
+			return step.ExitCodeFailure, nil, err
+		}
+	}
+
+	defer r.Body.Close()
+	respBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return step.ExitCodeFailure, nil, err
-	}
-
-	// response from `CreateEvent`: EventCreateResponse
-	rawRespBody, err := json.MarshalIndent(responseBody, "", "  ")
-	if err != nil {
-		return step.ExitCodeFailure, nil, err
-	}
-
-	//check HTTP error
-	if r.StatusCode != http.StatusAccepted {
-		return step.ExitCodeFailure, nil, fmt.Errorf("failed to create event. got response code: %d: %s",
-			r.StatusCode, string(rawRespBody))
-	}
-
-	var eventID int
-	if responseBody.Id != nil {
-		eventID = int(*responseBody.Id)
 	}
 
 	//prepare output
+	var eventID int64
+	if resp.Id != nil {
+		eventID = *resp.Id
+	}
+
 	stepOutput := output{
 		ID:      eventID,
-		Outputs: step.Outputs{Object: responseBody},
+		Outputs: step.Outputs{Object: string(respBody)},
 	}
+
 	jsonOutput, err := json.Marshal(stepOutput)
 	if err != nil {
 		return step.ExitCodeFailure, nil, err
